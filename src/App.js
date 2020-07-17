702fdf16bf81ce0fc3032cabcd961c686580c1fe
Robin Wieruch <wrobin@gmx.net>
2020-07-17 15:12:34 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello React Native!</Text>
+    </View>
+  );
+};
+
+export default App;
